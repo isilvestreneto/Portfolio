a10a4433a2832e988f746b21ec4a425af7dd4bf0
Neto <ivanildo.neto@capgemini.com>
2024-08-09 21:34:53 -0300

    wip header portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, CarouselModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
+
+  customOptions: OwlOptions = {
+    loop: true,
+    nav: true,
+    navText: ["Anterior", "Próximo"],
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  };
+
 }
---
+++ src/app/shared/models/project.ts
@@ -0,0 +1,6 @@
+export interface Project {
+    name: string;
+    description: string;
+    githubRepository: string;
+    tags: string[];
+}
